0d15cb3a170f1404dc89691d28d8152ce0956032
Thom Mott <thommott@proton.me>
2024-10-24 11:14:59 -0700

    Idle Timer

--- .config/hypr/idleBrightness.sh
+++ .config/hypr/idleBrightness.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-systemBrightnessFile="/sys/class/backlight/acpi_video0/brightness"
+systemBrightnessFile="/sys/class/backlight/nvidia_0/brightness"
 prevBrightnessFile="/tmp/prevBrightness.tmp"
 
 if [ "$1" = "startIdle" ]; then
-    cat $systemBrightnessFile >$prevBrightnessFile
-    cat $prevBrightnessFile
-    echo "0" | doas tee $systemBrightnessFile
+	cat $systemBrightnessFile >$prevBrightnessFile
+	cat $prevBrightnessFile
+	echo "0" | doas tee $systemBrightnessFile
 fi
 
 if [ "$1" = "endIdle" ]; then
-    cat $prevBrightnessFile | doas tee $systemBrightnessFile
+	cat $prevBrightnessFile | doas tee $systemBrightnessFile
 fi
 
 if [ "$1" = "finalIdle" ]; then
-    if ! pgrep -x "emerge" >/dev/null && ! pgrep -x "make" >/dev/null; then
-        loginctl suspend
-    fi
+	if ! pgrep -x "emerge" >/dev/null && ! pgrep -x "make" >/dev/null; then
+		loginctl suspend
+	fi
 fi
