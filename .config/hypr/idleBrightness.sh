3d939c604d51726642cecfdf895a6ec0950189d5
Thom Mott <thommott@proton.me>
2024-11-10 19:07:02 -0800

    Brightness script now respects AC power.

--- .config/hypr/idleBrightness.sh
+++ .config/hypr/idleBrightness.sh
@@ -3,6 +3,13 @@
 systemBrightnessFile="/sys/class/backlight/nvidia_0/brightness"
 prevBrightnessFile="/tmp/prevBrightness.tmp"
 
+AC_STATUS="/sys/class/power_supply/ADP0/online"
+
+if [ "$AC_STATUS" -eq 1 ]; then
+	cat $prevBrightnessFile | doas tee $systemBrightnessFile
+	exit 1
+fi
+
 if [ "$1" = "startIdle" ]; then
 	cat $systemBrightnessFile >$prevBrightnessFile
 	cat $prevBrightnessFile
